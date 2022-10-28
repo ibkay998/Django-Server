4ca06b97f0692933ad7fd7fd905ca2e57c1ba6c8
ibkay998 <ibukun46@gmail.com>
2022-10-28 14:40:08 +0100

    Update views.py

--- hng/views.py
+++ hng/views.py
@@ -2,10 +2,13 @@ from django.http import JsonResponse
 
 
 def index(request):
-    return JsonResponse({
+    response = JsonResponse({
         "slackUsername": "ibkay998",
         "backend":True,
         "age":23,
         "bio":"Hi my name is ibukunoluwa oyeniyi and an aspiring software developer interested both in frontend and backend and I love solving problems"
     })
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    return response
     
